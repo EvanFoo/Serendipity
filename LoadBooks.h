2931d8962a9fe67f6de4f05dcbb11caeb019139d
EvanFoo <efpepper22@gmail.com>
2015-05-27 22:16:33 -0700

    Added book struct to the module class

---
+++ LoadBooks.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   LoadBooks.h
+ * Author: evan
+ *
+ * Created on May 27, 2015, 10:12 PM
+ */
+
+#ifndef LOADBOOKS_H
+#define	LOADBOOKS_H
+
+#include "Module.h"
+
+class LoadBooks: public Module {
+public:
+    LoadBooks();
+    LoadBooks(const LoadBooks& orig);
+    virtual ~LoadBooks();
+private:
+
+};
+
+#endif	/* LOADBOOKS_H */
+
